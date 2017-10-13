12caf01e60a5faba3a0f80de1023a9ec440d3a76
kondfox <peter.tamas@greenfoxacademy.com>
2017-10-13 11:33:59 +0200

    feat(CountChars): add missing example txt file

--- countchars/CountChars.cs
+++ countchars/CountChars.cs
@@ -1,5 +1,6 @@
 // Create a function that takes the filepath (e.g. "da_vinci_code.txt") as an input, reads the given file, and counts the occurences of 0, 1 and x characters in it.
 // The function should return a dictionary in which the searched characters are the keys and the number of their occurences are the values.
+// Use the uploaded da_vinci_code.txt file in this folder on GitHub to test your program.
 // For example:
 //
 // var countedLetters = CountLetters("da_vinci_code.txt");
