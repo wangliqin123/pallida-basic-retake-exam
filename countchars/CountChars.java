3024b44ff4c0eee857dd4cdf5abff09c3bf95a85
aze <adam.gyulavari@lab.coop>
2017-10-15 15:27:20 +0200

    feat(python) add python exercises

--- countchars/CountChars.java
+++ countchars/CountChars.java
@@ -6,9 +6,9 @@ public class CountChars {
         // For example:
         //
         // Map<String, Integer> countedLetters = countLetters("da_vinci_code.txt");
-        // System.out.println("0 occured" + countedLetters.get("0") + " times in the file.");
-        // System.out.println("1 occured" + countedLetters.get("1") + " times in the file.");
-        // System.out.println("x occured" + countedLetters.get("x") + " times in the file.");
+        // System.out.println("0 occured " + countedLetters.get("0") + " times in the file.");
+        // System.out.println("1 occured " + countedLetters.get("1") + " times in the file.");
+        // System.out.println("x occured " + countedLetters.get("x") + " times in the file.");
         // 
         // Should print:
         // 0 occured 3393 times in the file.
---
+++ countchars/count_chars.py
@@ -0,0 +1,14 @@
+# Create a function that takes the filepath (e.g. "da_vinci_code.txt") as an input, reads the given file, and counts the occurences of 0, 1 and x characters in it.
+# The function should return a dictionary in which the searched characters are the keys and the number of their occurences are the values.
+# Use the uploaded da_vinci_code.txt file in this folder on GitHub to test your program.
+# For example:
+#
+# countedLetters = countLetters("da_vinci_code.txt");
+# print("0 occured " + countedLetters["0"] + " times in the file.");
+# print("1 occured " + countedLetters["1"] + " times in the file.");
+# print("x occured " + countedLetters["x"] + " times in the file.");
+# 
+# Should print:
+# 0 occured 3393 times in the file.
+# 1 occured 3375 times in the file.
+# x occured 3232 times in the file.
